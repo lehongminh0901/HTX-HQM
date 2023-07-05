239ee661382250eb6444e5c4e159407401c0f110
lehongminh0901 <75818488+lehongminh0901@users.noreply.github.com>
2023-07-05 08:54:40 +0700

    htx-hqm

---
+++ htx/htx/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using htx.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace htx.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Sanpham()
+        {
+            return View();
+        }
+        public IActionResult Tin()
+        {
+            return View();
+        }
+
+        public IActionResult Info()
+        {
+            return View();
+        }
+
+        public IActionResult Banhxeo()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
